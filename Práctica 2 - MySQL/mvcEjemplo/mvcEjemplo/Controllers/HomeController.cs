1abcfc1eecca8e2e272c523ad11703e49d3e1d45
Geovani de León <egdeleon@pensotec.com>
2018-08-30 18:15:09 -0600

    Ejemplo MySQL y PostgreSQL

---
+++ Práctica 2 - MySQL/mvcEjemplo/mvcEjemplo/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcEjemplo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<String> arrList = new List<string>();
+            arrList.Add("Zuñiga&#225;");
+            arrList.Add("Zuñiga&#233;");
+            arrList.Add("Zuñiga&#237;");
+            arrList.Add("Zuñiga");
+            arrList.Add("Zuñiga");
+            arrList.Add("Zuñiga");
+            ViewBag.lstApellidos = arrList;
+
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
